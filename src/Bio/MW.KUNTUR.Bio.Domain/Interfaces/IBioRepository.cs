ed98f55231e8e955826fa9038e197a7a85d9948e
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2024-05-13 22:46:05 -0500

    add domain repository interface

---
+++ src/Bio/MW.KUNTUR.Bio.Domain/Interfaces/IBioRepository.cs
@@ -0,0 +1,24 @@
+using MW.KUNTUR.Bio.Domain.Entities;
+
+namespace MW.KUNTUR.Bio.Domain.Interfaces;
+
+public interface IBioRepository : IDisposable
+{
+    Company GetCompanyById(int CompanyId, CancellationToken cancellationToken = default);
+    Company GetCompanyByJobId(int JobId, CancellationToken cancellationToken = default);
+    int CreateCompany(Company company, CancellationToken cancellationToken = default);
+    int CreateJob(Job job, CancellationToken cancellationToken = default);
+    int CreateProject(Project project, CancellationToken cancellationToken = default);
+    int CreateSocialNetwork(SocialNetwork socialNetwork, CancellationToken cancellationToken = default);
+    int CreateTechnology(Technology technology, CancellationToken cancellationToken = default);
+    IReadOnlyCollection<Job> GetJobsByPersonId(int PersonId, CancellationToken cancellationToken = default);
+    IReadOnlyCollection<Project> GetProjectsByPersonId(int PersonId, CancellationToken cancellationToken = default);
+    IReadOnlyCollection<SocialNetwork> GetAllSocialNetworks(CancellationToken cancellationToken = default);
+    IReadOnlyCollection<SocialNetwork> GetSocialNetworksByPersonId(int PersonId, CancellationToken cancellationToken = default);
+    IReadOnlyCollection<Technology> GetTechnologiesByProjectId(int ProjectId, CancellationToken cancellationToken = default);
+    Job GetJobById(int JobId, CancellationToken cancellationToken = default);
+    Person GetPersonAsync(int PersonId, CancellationToken cancellationToken = default);
+    Project GetProjectById(int ProjectId, CancellationToken cancellationToken = default);
+    SocialNetwork GetSocialNetworkById(int SocialNetworkId, CancellationToken cancellationToken = default);
+    Technology GetTechnologyById(int TechnologyId, CancellationToken cancellationToken = default);
+}
